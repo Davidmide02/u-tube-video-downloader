a3fdaa8120422423aa0600e4f8ed275699ed4df5
Davidmide <Davidmide07@gmail.com>
2024-05-14 04:50:37 +0100

    download func and page

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pytube import YouTube
 
 
 app = Flask(__name__)
 
-@app.route('/')
+def download(url):
+    if url :
+        try:
+            yt = YouTube(url)
+            return f'{yt.title} Downloaded successfully'
+        except Exception as e:
+            return f'error message :{e}'
+        
+        
+@app.route('/', methods=['POST', 'GET'])
 def index():
-   return render_template('index.html')
+    if request.method =='POST':
+        url = request.form.get('url')
+        print(url)
+        vid = download(url)
+        print(vid)
+        return f'loader here: {url}'
+    return render_template('index.html')
 
 
 
