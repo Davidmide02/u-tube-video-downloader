5efb78b5de72ff633e31cb1a3bf3713dec7164b1
Davidmide <Davidmide07@gmail.com>
2024-05-24 05:03:27 +0100

    try sending video to browser

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, send_from_directory
 from pytube import YouTube
 
 
 app = Flask(__name__)
 
+
+@app.route('/download/title')
 def download(url):
     if url :
         try:
             yt = YouTube(url)
-            return f'{yt.title} Downloaded successfully'
+            title = yt.title
+            stream = yt.streams.filter(only_video=True).first()
+            stream.download(output_path='downloads')
+            return f'{yt.title} {send_from_directory('downloads',title)} Downloaded successfully'
         except Exception as e:
             return f'error message :{e}'
         
@@ -18,8 +23,7 @@ def index():
     if request.method =='POST':
         url = request.form.get('url')
         print(url)
-        vid = download(url)
-        print(vid)
+        download(url)
         return f'loader here: {url}'
     return render_template('index.html')
 
@@ -27,3 +31,7 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    # download progress indicator
+    # send file to browser
+    #  https://youtu.be/YERdEaEoy5E
